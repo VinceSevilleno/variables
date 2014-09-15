b6a930934a5ca1e80f924b497106586e100bbb5e
VinceSevilleno <44963@longroad.ac.uk>
2014-09-15 10:12:53 +0100

    first commit

---
+++ revision exercise 1.py
@@ -0,0 +1,11 @@
+#Vince Sevilleno
+#15-09-2014
+#Revision Exercise
+
+print("Give four integers and the total of all added together will be given.")
+input_number=int(input("Please enter the first number:"))
+input_number2=int(input("Please enter the second number:"))
+input_number3=int(input("Please enter the third number:"))
+input_number4=int(input("Please enter the fourth number:"))
+answer= input_number+input_number2+input_number3+input_number4
+print("The total of the numbers added is {0}".format(answer))

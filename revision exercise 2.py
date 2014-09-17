8548c7e745151d47e439a9b8d942221a029ef5c3
VinceSevilleno <44963@longroad.ac.uk>
2014-09-17 15:16:19 +0100

    revision exercise 2 and 3

---
+++ revision exercise 2.py
@@ -0,0 +1,14 @@
+#Vince Sevilleno
+#17-09-2014
+#Revision Exercise 2
+
+print("Give two numbers which will make product of the integers, then give a final number to divide the product with.")
+number_1=int(input("Please enter the first number:"))
+number_2=int(input("Please enter the second number:"))
+product=number_1*number_2
+print("The product of the two integers is {0}".format(product))
+number_3=int(input("Please enter the third number which will be used to divide the product with:"))
+result=product/number_3
+print("The final answer is {0}".format(product))
+
+                   
---
+++ revision exercise 3.py
@@ -0,0 +1,14 @@
+#Vince Sevilleno
+#17-09-2014
+#Revision Exercise 3
+
+print("Give two integers and this program will divide the integers and give the number of times one goes into the other and its remainder.")
+print()
+number1=int(input("Please enter the first number:"))
+print()
+number2=int(input("Please enter the second number:"))
+answer=number1//number2
+remainder=number1%number2
+print()
+print("The answer is {0} and the remainder is {1}".format(answer,remainder))
+

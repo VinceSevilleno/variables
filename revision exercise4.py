0654fd9cd0b625a550e0d9525adc2c7a06b0b681
VinceSevilleno <44963@longroad.ac.uk>
2014-09-22 09:33:10 +0100

    revision exercise 4

---
+++ revision exercise4.py
@@ -0,0 +1,12 @@
+#Vince Sevileno
+#22/09/2014
+#Revision Exercise 4
+
+print("Give two integers and it will divide them through floor division which shows just the whole number:")
+print()
+number1=int(input("Please enter the first number:"))
+print()
+number2=int(input("Please enter the second numner:"))
+print()
+answer=number1//number2
+print("The answer is {0}.".format(answer))

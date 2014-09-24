211d1e2dc55ffbcd461a6e3c6389106994f11d06
VinceSevilleno <44963@longroad.ac.uk>
2014-09-24 15:03:48 +0100

    developmentexercise 1

---
+++ developmentexercise1.py
@@ -0,0 +1,13 @@
+#Vince Sevilleno
+#24/09/2014
+#Development Exercise 1
+
+print("Give two values for X and Y, X will be divided by Y and give the answer that shows its integer and its remainder:")
+print()
+inputX=int(input("Give a value for X:"))
+print()
+inputY=int(input("Give a value for Y:"))
+print()
+integer=inputX//inputY
+remainder=inputX%inputY
+print("The answer is {0}.{1}".format(integer,remainder))

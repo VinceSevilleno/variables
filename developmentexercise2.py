5308adef33a1ac2baf8b4a51c370297d829daaaa
VinceSevilleno <44963@longroad.ac.uk>
2014-09-24 15:11:02 +0100

    development exercise 2

---
+++ developmentexercise2.py
@@ -0,0 +1,10 @@
+#Vince Sevilleno
+#24/09/2014
+#Development Exercise 2
+
+print("This program will convert a given value in  degrees Fahrenheit into degrees Centigrade:")
+print()
+fahrenheit=int(input("Please enter any value of temperature:"))
+centigrade=(fahrenheit-32)*(5/9)
+print()
+print("The value {0} degrees Fahrenheit is equal to {1} degrees Centigrade.".format(fahrenheit,centigrade))
